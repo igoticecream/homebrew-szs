25864fd119e9a671c32ceb70347b35169fd07142
Pedro Diaz <igoticecream@gmail.com>
2019-07-29 14:52:04 +0200

    Update szs.rb

--- szs.rb
+++ szs.rb
@@ -17,15 +17,6 @@ class Szs < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test szs-v`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    assert_equal %x('#{bin}/wszst'), "wszst: Wiimms SZS Tool v1.64a r7680 mac - Dirk Clemens - 2018-11-23\n"
   end
 end
